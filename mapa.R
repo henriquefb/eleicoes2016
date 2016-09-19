aeae534e5c4299fdc8b202b377f289cf0e72a985
omercadopopular <andregoes@gmail.com>
2016-09-19 17:52:00 -0400

    Update mapa.R

--- mapa.R
+++ mapa.R
@@ -13,24 +13,32 @@ baseline <- read.csv(file="https://raw.githubusercontent.com/omercadopopular/ele
 
 partidos = 26
 
+brazilmap <-  get_map(
+  "Brazil",
+  zoom = 4,
+  color="bw"
+)
+
 lista <- names(baseline[,1:partidos])
+
 
 for (legenda1 in lista) {
   for (legenda2 in lista) {
-    assign(paste(legenda1,legenda2, sep = "") , subset(baseline, legenda1 == 1 & legenda2 == 1) )
-      }
-}
+#    assign(paste(legenda1,legenda2, sep = "") , 
+    assign(
+      paste("m",legenda1,legenda2, sep = ""),
+      ggmap(brazilmap, extent = "device") +
+        geom_point(
+               aes(x = longitude, y = latitude),
+               color = 'red',
+               data = baseline[baseline[,legenda1] == 1 & baseline[,legenda2] == 1,]
+               , size = 0.5)
+      )
+    ggsave(
+      paste("m",legenda1,legenda2, sep = ""),
+      filename = paste("m",legenda1,legenda2,".png",sep = ""),
+      dpi = 600)     
+  }
+}  
 
-
-brazilmap <-  get_map(
-    "Brazil",
-    zoom = 4,
-    color="bw"
-    )
-  
-sample1 <- subset(baseline, PMDB == 1 & PT == 1)
-
-ggmap(brazilmap, extent = "device") + geom_point(aes(x = longitude, y = latitude), size = 1 , color = 'red' , data = sample1, size = 0.5)
-
-ggsave("map.png", dpi = 600) 
 
