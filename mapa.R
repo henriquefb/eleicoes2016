da1ddad069c411c02e252fc5992da2b9398b22a4
omercadopopular <andregoes@gmail.com>
2016-09-26 18:06:19 -0400

    Update mapa.R

--- mapa.R
+++ mapa.R
@@ -1,6 +1,24 @@
+#///////////////////////////////////////////////////////////////////////////////
+#///////////////////////////////// ESCRITO POR /////////////////////////////////
+#///////////////////////////////// CARLOS GÓES /////////////////////////////////
+#////////////////////////////////////// E //////////////////////////////////////
+#//////////////////////////////// RADUAN MEIRA /////////////////////////////////
+#///////////////////////////////////////////////////////////////////////////////
+#///////////////////////// INSTITUTO MERCADO POPULAR ///////////////////////////
+#/////////////////////////// www.mercadopopular.org ////////////////////////////
+#///////////////////////////////////////////////////////////////////////////////
 
-# Escrito por Carlos Góes
-# Pesquisador-Chefe, Instituto 
+#///////////////////////////////////////////////////////////////////////////////
+#////////////////////////////////// LEIA-ME ////////////////////////////////////
+#///////////////////////////////////////////////////////////////////////////////
+  
+  
+#  Descrição do projeto
+
+# Usa dados do arquivo prefeito.csv e o conjunto de coordenadas
+# geo-espaciais para criar um mapa das alianças entre os partidos.
+
+#
 
 setwd("U:/Research/partidos/basededados/")
 
@@ -9,36 +27,37 @@ library(ggmap)
 library(maps)
 library(mapproj)
 
-baseline <- read.csv(file="https://raw.githubusercontent.com/omercadopopular/eleicoes2016/master/prefeito.csv",head=TRUE,sep=",")
+baseline <- read.csv(file="U:/Research/partidos/basededados/prefeito.csv",head=TRUE,sep=",")
 
-partidos = 26
 
 brazilmap <-  get_map(
   "Brazil",
   zoom = 4,
-  color="bw"
+  color="bw",
+  maptype ="hybrid"
 )
 
 lista <- names(baseline[,1:partidos])
 
 
-for (legenda1 in lista) {
-  for (legenda2 in lista) {
-#    assign(paste(legenda1,legenda2, sep = "") , 
-    assign(
-      paste("m",legenda1,legenda2, sep = ""),
-      ggmap(brazilmap, extent = "device") +
-        geom_point(
-               aes(x = longitude, y = latitude),
-               color = 'red',
-               data = baseline[baseline[,legenda1] == 1 & baseline[,legenda2] == 1,]
-               , size = 0.5)
-      )
-    ggsave(
-      paste("m",legenda1,legenda2, sep = ""),
-      filename = paste("m",legenda1,legenda2,".png",sep = ""),
-      dpi = 600)     
-  }
-}  
-
-
+pmdbmapa <- ggmap(brazilmap, extent = "device") + 
+ geom_point(
+    aes(x = longitude, y = latitude),
+    color = 'blue',
+    data = baseline[baseline[,"PMDB"] == 1 & baseline[,"PSDB"] == 1,],
+    size = 1,
+    alpha = 1) +
+  geom_point(
+    aes(x = longitude, y = latitude),
+    color = 'red',
+    data = baseline[baseline[,"PMDB"] == 1 & baseline[,"PT"] == 1,],
+    size = 1,
+    alpha = 1) +
+geom_point(
+  aes(x = longitude, y = latitude),
+  color = 'white',
+  data = baseline[baseline[,"PMDB"] == 1 & baseline[,"PSDB"] == 1 & baseline[,"PT"] == 1,],
+  size = 1,
+  alpha = 1)
+  
+ggsave(pmdbmapa,   filename = "pmdbmapa.pdf")       
